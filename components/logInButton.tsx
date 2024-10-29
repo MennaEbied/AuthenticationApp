db4ad0aa7ab4784ab95f4e2155076e2e9d5e5fe8
MennaEbied <mennahesham253@gmail.com>
2024-10-29 22:14:27 +0300

    Customize button

---
+++ components/logInButton.tsx
@@ -0,0 +1,34 @@
+import { theme } from "../theme";
+import { StyleSheet, Text, Pressable } from "react-native";
+
+type Props = {
+  title: string;
+  onPress: () => void;
+};
+
+export function LogInButton({ title, onPress }: Props) {
+  return (
+    <Pressable onPress={onPress} style={styles.button}>
+      <Text style={styles.text}>{title}</Text>
+    </Pressable>
+  );
+}
+
+const styles = StyleSheet.create({
+  text: {
+    color: theme.colorWhite,
+    fontSize: 12,
+    fontWeight: "bold",
+    textAlign:"center",
+  },
+  button: {
+    paddingHorizontal: 18,
+    paddingVertical: 12,
+    borderRadius: 6,
+    backgroundColor:theme.colorBlack,
+    width:200,
+    height:40,
+    marginBottom:10,
+   
+  },
+});
